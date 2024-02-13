8d9826773569b2b44b22bed6807e56bc4659f65c
Wipamas Polpakdee <wipamas.polpakdee@gmail.com>
2024-02-13 16:29:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,13 @@ const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
   question.innerHTML = "YES!! Thank you ^ ^";
-  gif.src =
-    "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+  gif.src = "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExd2s1OTc3eGcyeXZsanB3cGFlbGZuMnFmbWdocDRpcnhkOGRkbXV1cCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/xWTNfEu9uAXBnFQIa5/giphy.gif";
 });
 
 noBtn.addEventListener("mouseover", () => {
   question.innerHTML = "Are you sure?";
+  gif.src = "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMjU0N2xldzlnZ3FrbjA3Y3Q5bnJsbTA5em53ZHludHdheDN0cXR4aCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/huPOZgjL4SWzLkJwH2/giphy.gif";
+  
   const noBtnRect = noBtn.getBoundingClientRect();
   const maxX = window.innerWidth - noBtnRect.width;
   const maxY = window.innerHeight - noBtnRect.height;
